259e478b8b6478c1c0368129d763f4317a6a3b00
atsougan870@gmail.com <atsougan870@gmail.com>
2024-10-10 11:35:51 +0300

    Update .tsx to .jsx

--- app/index.jsx
+++ app/index.jsx
@@ -1,12 +1,15 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>This is wonderful</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.container}>
+      <StatusBar style="auto"  />
+      <Text>This is wonderful, buddy!</Text>
+      
+    
+    </SafeAreaView>
   );
 }
 
