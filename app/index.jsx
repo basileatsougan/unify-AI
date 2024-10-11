b3081db3e976f70ec9477efb2f5c81b5337c4b31
atsougan870@gmail.com <atsougan870@gmail.com>
2024-10-11 06:46:57 +0300

    Add fonts

--- app/index.jsx
+++ app/index.jsx
@@ -3,11 +3,13 @@ import { Text, View } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import { Link } from 'expo-router'
 
+
+
 export default function App() {
   return (
     <SafeAreaView className="flex-1 items-center justify-center bg-white">
       <StatusBar style="auto"  backgroundColor='#161212' color='white'/>
-      <Text className="text-3xl">This is wonderful, buddy!</Text>
+      <Text className="text-3xl">Aora!</Text>
       <Link href="/profile" style={{ color: 'bue'}}>Go to profile</Link>
     
     </SafeAreaView>
