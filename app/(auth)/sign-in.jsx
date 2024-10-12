341511e7df49f3d451bdaa92635f6e4132e39159
atsougan870@gmail.com <atsougan870@gmail.com>
2024-10-12 14:22:12 +0300

    Add the Sign In page

--- app/(auth)/sign-in.jsx
+++ app/(auth)/sign-in.jsx
@@ -2,9 +2,10 @@ import { View, Text, ScrollView, Image } from 'react-native'
 import React, { useState } from 'react'
 import { images } from '../../constants'
 import { SafeAreaView } from 'react-native-safe-area-context';
+import { Link } from 'expo-router';
 
 import FormField from '../../components/FormField';
-
+import CustomButton from '../../components/CustomButton';
 const signIn = () => {
 
   const [form, setForm] = useState({
@@ -12,10 +13,17 @@ const signIn = () => {
     password: ''
   })
 
+  const [isSubmitting, setIsSubmitting] = useState(false);
+
+  const submit = () => {
+
+  }
+
+
   return (
     <SafeAreaView className="bg-primary h-full">
       <ScrollView>
-        <View className="w-full justify-content items-center h-full px-4 my-4">
+        <View className="w-full justify-center min-h-[85px] px-4 my-4">
           <Image 
             source={images.logo}
             resizeMode='contain'
@@ -38,6 +46,22 @@ const signIn = () => {
             handleChangeText={(e) => setForm({ ...form, password: e})}
             otherStyles="mt-7"
           />
+
+          <CustomButton 
+            title="Sign In"
+            handlePress={submit}
+            containerStyles="mt-7"
+            isLoading={isSubmitting}
+
+          />
+
+          <View className="justify-center pt-5 flex-row gap-2">
+            <Text className="text-lg text-gray-100 font-pregular">
+              Don’t have an account?
+            </Text>
+            <Link href="/sign-up" className="text-lg text-secondary font-semibold">Signup</Link>
+          </View>
+
         </View>
       </ScrollView>
     </SafeAreaView>
